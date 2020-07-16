4f73e85d4e2340cad451e76407caf68cb79e9670
Dedalus <dias.julien945@gmail.com>
2020-07-16 09:02:09 +0200

    add search tags

--- assets/filter.js
+++ assets/filter.js
@@ -1,5 +1,5 @@
 const urlParams = new URLSearchParams(window.location.search);
-const search = urlParams.get('search');
+let search;
 
 function isRelevant(elt){
     return hasCategory(elt) || hasTags(elt) || hasTitle(elt) || hasAuthor(elt);
@@ -16,89 +16,59 @@ function hasTitle(elt){
 function hasAuthor(elt){
     return elt.author == search;
 }
+function generateHtml(filtered){
 
-
-// Récupération asynchrone des données json
-fetch('/search.json')
-    .then(res => res.json())
-    .then((data) => {
-        console.log(data)
-        const allPosts = data.posts.concat(data.codes);
-        const filtered = allPosts.filter(isRelevant);
-        console.log(filtered)
-
-        // Le reste du code va ici
-        // ...
-
-        const $content = document.getElementById("dynamic-content");
-        $content.innerHTML = generateHtml();
-
-        function generateHtml(){
-
-            let html="";
-            for(let datum of filtered){
-                html +=`
-                <div class="col-12 postlist alternative text-center">
-                <a href="${ datum.url }" class="url-title"><h2>${ datum.title }</h2></a>
-                
-                
-                    <p class="row ">
-                        <div class="col">
-                         <img class=" w-25 " src="${ datum.image }" alt="${ datum.title }"> 
-                        </div>
-                    </p>
-                    <br/>
-                ${ datum.content }
-                <br/>
-                ${ datum.author }
-                <br/>
-                    ${
-                        datum.tags.split(',').map(function(tag){
-                            return `
-                            <a href="/tag.html?tag=${ tag }">
-                                ${ tag }
-                            </a>
-                            `;
-                        }).join('')
-                    }
+    let html="";
+    for(let datum of filtered){
+        html +=`
+        <div class="col-12 postlist alternative text-center">
+        <a href="${ datum.url }" class="url-title"><h2>${ datum.title }</h2></a>
+        
+        
+            <p class="row ">
+                <div class="col">
+                 <img class=" w-25 " src="${ datum.image }" alt="${ datum.title }"> 
                 </div>
-                
-                `
-            }
+            </p>
+            <br/>
+        ${ datum.content }
+        <br/>
+        ${ datum.author }
+        <br/>
+        `;
 
-            return html;
-        }function generateHtml(){
-
-            let html="";
-            for(let datum of filtered){
-                html +=`
-                <div class="col-12 postlist alternative text-center">
-                <a href="${ datum.url }" class="url-title"><h2>${ datum.title }</h2></a>
-                
-                
-                    <p class="row ">
-                        <div class="col">
-                         <img class=" w-25 " src="${ datum.image }" alt="${ datum.title }"> 
-                        </div>
-                    </p>
-                    <br/>
-                ${ datum.content }
-                <br/>
-                ${ datum.author }
-                <br/>
-                `;
-
-                for(let tag of datum.tags.split(',')){
-                    html += `
-                            <a href="/tag.html?tag=${ tag }">
-                                ${ tag }
-                            </a>
-                            `;
-                }
-
-                html += "</div>";
-            }
-
-            return html;
+        for(let tag of datum.tags.split(',')){
+            html += `
+                    <a href="/tag.html?tag=${ tag }">
+                        ${ tag }
+                    </a>
+                    `;
         }
-    })
+
+        html += "</div>";
+    }
+
+    return html;
+}
+
+function generateResults(mode){
+    // Récupération asynchrone des données json
+    fetch('/search.json')
+        .then(res => res.json())
+        .then((data) => {
+            const allPosts = data.posts.concat(data.codes);
+            let filterFunction;
+            
+            if(mode == "search"){
+                filterFunction = isRelevant;
+                search = urlParams.get('search');
+            }else{
+                filterFunction = hasTags;
+                search = urlParams.get('tag');
+            }
+            const filtered = allPosts.filter(filterFunction);
+
+            const $content = document.getElementById("dynamic-content");
+            $content.innerHTML = generateHtml(filtered);
+        })
+}
